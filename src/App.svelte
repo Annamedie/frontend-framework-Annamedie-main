b26324d1178c76117c3338b2d1af518d9e253cbd
annaa <annaalehed@hotmail.com>
2024-02-08 14:23:03 +0100

    Startar upp svelte projekt

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const years = [2016,2017,2018,2019,2020,2021,2022]
+</script>
+<header>
+<h1>
+  The state of Javascript
+</h1>
+</header>
+<style>
+  h1{
+    display: flex;
+    justify-content: center;
+    color: white;
+    font-size: 5em;
+  }
+</style>
