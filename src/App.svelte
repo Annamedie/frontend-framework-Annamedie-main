d48ca13b6ce8b75297378d7c581b26a670ebda6b
annaa <annaalehed@hotmail.com>
2024-02-09 10:43:31 +0100

    testar

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
-  const years = [2016,2017,2018,2019,2020,2021,2022]
+  const years = [2016,2017,2018,2019,2020,2021,2022];
 </script>
 <header>
 <h1>
   The state of Javascript
 </h1>
 </header>
+<main>
+  <ul class="years">
+    {#each years as year}
+      <li>{year}</li>
+    {/each}
+  </ul>
+</main>
 <style>
   h1{
     display: flex;
     justify-content: center;
     color: white;
     font-size: 5em;
+  }
+  li{
+    color: white;
   }
 </style>
