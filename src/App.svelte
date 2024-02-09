e3880242b75efccc2efd592fb6919c1ab65248c0
annaa <annaalehed@hotmail.com>
2024-02-09 15:54:30 +0100

    refactor och grid
    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { frameworks } from '../data/index.js';
+
+  const url = "awareness";
 </script>
 <header>
 <h1>
@@ -7,23 +9,33 @@
 </h1>
 </header>
 <main>
-  <ul class="years">
+  <ul data-cy="chart" class="chart">
+    <li data-cy="chart-header-cell"></li>
+    {#each frameworks[0].surveys as survey}
+      <li data-cy="chart-header-cell">{survey.year}</li>
+    {/each}
+    <li data-cy="chart-header-cell"></li>
+
     {#each frameworks as {name, color, surveys }}
-      <h2 style="color: {color};">{name}</h2>
-      <ul>
-        {#each surveys as {year, retention } }
-        <li >
-          {year}
-        </li>
-        <li>{retention}%</li>
-          
+      <h2 data-cy="chart-data-cell" style="color: {color};">{name}</h2>
+
+        <!-- Skapa tomma celler för de undersöknar som saknas: surveys[0].year - 2016 = 3 -->
+      
+        {#each surveys as survey }
+          <li data-cy="chart-data-cell">
+            <div data-cy="chart-circle">{survey[url]}%</div>
+          </li>    
         {/each}
-      </ul>
-      <h2 style="color: {color};">{name}</h2>
+      
+      <h2 data-cy="chart-data-cell" style="color: {color};">{name}</h2>
     {/each}
   </ul>
 </main>
 <style>
+  .chart {
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+  }
   h1{
     display: flex;
     justify-content: center;
