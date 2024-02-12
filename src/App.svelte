c9c9e0860996f9d4e4ff74143d49fa7c122f8bb3
annaa <annaalehed@hotmail.com>
2024-02-12 01:31:43 +0100

    kinda looks likte the picture now lot of css

--- src/App.svelte
+++ src/App.svelte
@@ -10,14 +10,15 @@
 </header>
 <main>
   <div data-cy="chart" class="chart">
-    <div data-cy="chart-header-cell"></div>
+    
+    <div data-cy="chart-header-cell" class="header-cell"></div>
     {#each frameworks[0].surveys as survey}
-      <div data-cy="chart-header-cell">{survey.year}</div>
+      <div data-cy="chart-header-cell" class="header-cell">{survey.year}</div>
     {/each}
-    <div data-cy="chart-header-cell"></div>
-
+    <div data-cy="chart-header-cell" class="header-cell"></div>
+  
     {#each frameworks as {name, color, surveys }}
-      <h2 data-cy="chart-data-cell" style="color: {color};">{name}</h2>
+      <h2 data-cy="chart-data-cell" class= "data-cell-name" style="color: {color};">{name}</h2>
 
         <!-- Skapa tomma celler för de undersöknar som saknas: surveys[0].year - 2016 = 3 -->
         {#each {length: surveys[0].year - 2016} as _,i }
@@ -25,19 +26,26 @@
         {/each}
       
         {#each surveys as survey }
-          <li data-cy="chart-data-cell">
-            <div data-cy="chart-circle">{survey[url]}%</div>
-          </li>    
+          <div data-cy="chart-data-cell" style= "border-color: {color};"class="chart-data">
+            <div data-cy="chart-circle" style= "color: {color}" >{survey[url]}%</div>
+          </div>    
         {/each}
       
-      <h2 data-cy="chart-data-cell" style="color: {color};">{name}</h2>
+      <h2 data-cy="chart-data-cell" class= "data-cell-name" style="color: {color};">{name}</h2>
     {/each}
         </div>
 </main>
 <style>
+ 
   .chart {
     display: grid;
     grid-template-columns: repeat(9, 1fr);
+    gap: 10px;
+    justify-items: center;
+    align-items: center;
+    margin: auto;
+    overflow-x: scroll;
+   
   }
   h1{
     display: flex;
@@ -48,15 +56,16 @@
   h2{
     color: white;
   }
-  li{
-    list-style: none;
-    color: white;
-  }
-  div{
-    color: azure;
-    width: 30px;
-    height: 30px;
-    border:1px solid blue;
+  .chart-data{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    width: 50px;
     border-radius: 50%;
+    border-style: solid;
   }
+
+
+ 
 </style>
